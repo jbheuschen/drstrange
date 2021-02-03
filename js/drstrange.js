41c89a54cbfa192bf0e16370b644105124853cc0
Julian Benedikt Heuschen <julian@fheuschen.de>
2021-02-03 23:08:45 +0100

    Fullscreenable Card

--- js/drstrange.js
+++ js/drstrange.js
@@ -1,1 +1,14 @@
 var rellax = new Rellax(".parallax")
+
+$(document).ready(function() {
+   $(".fullscreenable").click(function(e) {
+      let l = $($(this).data('fullscreen'));
+      if(l.hasClass('fullscreened')) {
+          l.fadeOut();
+      } else {
+          l.fadeIn();
+      }
+      l.toggleClass('fullscreened');
+      $("#content").toggleClass("blurred")
+   });
+});
