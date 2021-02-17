65e23eb2b40489fe6ee4f093b19b6e677a5a084e
Julian Benedikt Heuschen <julian@fheuschen.de>
2021-02-17 08:59:07 +0100

    Opener Scroll Behavior

--- js/drstrange.js
+++ js/drstrange.js
@@ -14,4 +14,20 @@ $(document).ready(function() {
    $(".parallax").each(function(i) {
       $(this).css('background-image', 'url(' + $(this).data('img') + ')')
    });
+   $("#scrolldown").click(function(e) {
+      $("body,html").animate({
+         scrollTop: $("#content").offset().top
+      },800, "swing", function() {
+         $("body").removeClass("no-scroll");
+      })
+   });
+   $(window).scroll(function() {
+      if($(this).scrollTop() <= $("#opener").height() && !$("body").hasClass("no-scroll")) {
+         $("body,html").animate({
+            scrollTop: 0
+         },800, "swing", function() {
+            $("body").addClass("no-scroll");
+         })
+      }
+   })
 });
